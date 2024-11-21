0d0120b20e914061a0b902fe295a199069fec40f
Ashutosh Shukla <ashutosh6471@gmail.com>
2024-11-21 11:46:48 +0530

    Initial Project Setup Completed

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from './assets/logo.png' ;
+
+/**
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search`
+ * - RestaurantContainer
+ *   - RestaurantCard
+ * Footer
+ * - Copywright
+ * - Links
+ */
+const Header = ()  =>{
+    return (
+        <div className="header">
+            <div>
+                <img className="logo" src={logo}/>
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+
+                </ul>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+const AppLayout = () =>{
+    return (
+        <div className="app">
+            <Header/>
+
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
